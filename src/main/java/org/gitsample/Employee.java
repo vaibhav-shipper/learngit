2147b38534bb2ed47e55f0186cbc32a1b07429fb
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2020-07-25 09:36:30 +0000

    Added getters and setters

--- src/main/java/org/gitsample/Employee.java
+++ src/main/java/org/gitsample/Employee.java
@@ -3,4 +3,18 @@ package org.gitsample;
 public class Employee {
 	private String name;
 	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 }
