d43c9f25c42900d3982c127b53e57177388f4bea
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2020-07-27 08:01:02 +0000

    Remove fields for test

--- src/main/java/org/gitsample/Main.java
+++ src/main/java/org/gitsample/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test Main !! ");
 		System.out.println("Hi this is a change !!");
-		System.out.println("Hi this is a change again !!");
 	}
 
 }
