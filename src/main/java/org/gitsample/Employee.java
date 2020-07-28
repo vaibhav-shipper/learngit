82ffce589f00b8b9a02376e0442e1d2665db8bb2
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2020-07-28 12:44:22 +0000

    Removed all getters and setters

--- src/main/java/org/gitsample/Employee.java
+++ src/main/java/org/gitsample/Employee.java
@@ -1,30 +1,6 @@
 package org.gitsample;
 
 public class Employee {
-	//New comment added
 	private String name;
 	private int age;
-	private int salary;
-	private String city;
-
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	@Override
-	public String toString() {
-		return "Employee [name=" + name + ", age=" + age + "]";
-	}
-	
-	
-	
-	
 }
