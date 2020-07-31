27acea5566ab7e47d41999687eae152b30469f67
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2020-07-31 15:26:58 +0000

    Prod Fix 1

--- src/main/java/org/gitsample/Company.java
+++ src/main/java/org/gitsample/Company.java
@@ -3,6 +3,11 @@ package org.gitsample;
 public class Company {
 	private String name;
 	private String code;
+	@Override
+	public String toString() {
+		return "Company [name=" + name + ", code=" + code + "]";
+	}
+	
 	
 	
 }
