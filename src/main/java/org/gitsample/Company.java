132f277b96675d3c7fc24764e5d6200110e1c5af
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2020-08-02 13:40:57 +0530

    Made changes in Company and Main again

--- src/main/java/org/gitsample/Company.java
+++ src/main/java/org/gitsample/Company.java
@@ -3,6 +3,7 @@ package org.gitsample;
 public class Company {
 	private String name;
 	private String address;
+	private String code ;
 	@Override
 	public String toString() {
 		return " [name=" + name + ", address=" + address + "]";
--- src/main/java/org/gitsample/Main.java
+++ src/main/java/org/gitsample/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test Main !! ");
 		System.out.println("Hi this is a change !!");
+		System.out.println("Hi this is a change again !!");
 		
 	}
 
