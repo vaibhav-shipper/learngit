aa9647571c51ab189b9e65daf2fc82aadf3d3122
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2020-08-03 12:29:59 +0530

    Removed field code

--- src/main/java/org/gitsample/Company.java
+++ src/main/java/org/gitsample/Company.java
@@ -3,10 +3,15 @@ package org.gitsample;
 public class Company {
 	private String name;
 	private String address;
-	private String code ;
 	@Override
 	public String toString() {
 		return " [name=" + name + ", address=" + address + "]";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 	
