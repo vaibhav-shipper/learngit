d423930040869a9d7e198d78674a29ac96a7edf6
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2020-08-07 18:49:21 +0530

    Company change version 1.0

--- src/main/java/org/gitsample/Company.java
+++ src/main/java/org/gitsample/Company.java
@@ -2,27 +2,5 @@ package org.gitsample;
 
 public class Company {
 	
-	//Change for patch 1
 	private String name;
-	//Change 2
-	
-	private String city;
-	private int salary;
-	
-	@Override
-	public String toString() {
-		return "Company [name=" + name + ", city=" + city + ", salary=" + salary + "]";
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
-	
-	
-	
-	
 }
