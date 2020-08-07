a337efdd3251ecb97feed8a8f7255d8de2d0a877
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2020-08-07 18:50:58 +0530

    Company change version 3.0

--- src/main/java/org/gitsample/Company.java
+++ src/main/java/org/gitsample/Company.java
@@ -4,4 +4,17 @@ public class Company {
 	
 	private String name;
 	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
 }
