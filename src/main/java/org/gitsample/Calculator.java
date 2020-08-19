cf27276a92321ca54cb6989a2f3d1a0d7186ab06
gitdummyuser2020 <gitdummyuser2020@gmail.com>
2020-08-19 20:58:37 +0530

    Feature Added - Addition and Subtraction

--- src/main/java/org/gitsample/Calculator.java
+++ src/main/java/org/gitsample/Calculator.java
@@ -1,23 +1,16 @@
 package org.gitsample;
 
 public class Calculator {
-	//Change 2
-	//Change 2 feature
 
-	
 	public static int add(int num1, int num2) {
 		int sum = num1 + num2;
 		System.out.println("num1:" + num1 + ",num2:" + num2 + ",sum:" + sum);
 		return sum;
 	}
-
-	public static int subtract(int num1, int num2) {
-		return (num1 - num2);
-	}
 	
-	public static int multiply(int num1,int num2)
-	{
-		return (num1*num2);
+	public static int subtract(int num1, int num2) {
+		int sum = num1 - num2;
+		System.out.println("num1:" + num1 + ",num2:" + num2 + ",sum:" + sum);
+		return sum;
 	}
-
 }
