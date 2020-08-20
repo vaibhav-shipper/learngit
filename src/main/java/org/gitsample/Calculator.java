135f1241a071f2b99d4bccc7297f83e6bbfd2561
gitdummyuser2020 <gitdummyuser2020@gmail.com>
2020-08-20 21:59:11 +0530

    Code cleanup

--- src/main/java/org/gitsample/Calculator.java
+++ src/main/java/org/gitsample/Calculator.java
@@ -14,13 +14,4 @@ public class Calculator {
 		return sum;
 	}
 	
-	public static int multiply(int num1,int num2)
-	{
-		return (num1*num2);
-	}
-	
-	public static int divide(int num1,int num2)
-	{
-		return (num1/num2);
-	}
 }
