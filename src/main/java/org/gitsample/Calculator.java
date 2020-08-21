1b1063c92a0362847b79015e31c48ad87daf2b10
gitdummyuser2020 <69079226+gitdummyuser2020@users.noreply.github.com>
2020-08-22 01:24:13 +0530

    Change 2

--- src/main/java/org/gitsample/Calculator.java
+++ src/main/java/org/gitsample/Calculator.java
@@ -2,6 +2,7 @@ package org.gitsample;
 
 public class Calculator {
 
+	//Change 2
 	public static int add(int num1, int num2) {
 		int sum = num1 + num2;
 		System.out.println("num1:" + num1 + ",num2:" + num2 + ",sum:" + sum);
