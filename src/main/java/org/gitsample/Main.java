a2aa7ea73ee71ed6893826af8f9a0695731c1663
vaibhav-shipper <75312251+vaibhav-shipper@users.noreply.github.com>
2021-02-21 18:11:38 +0530

    Modified Main.java

--- src/main/java/org/gitsample/Main.java
+++ src/main/java/org/gitsample/Main.java
@@ -3,10 +3,10 @@ package org.gitsample;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test Main !! ");
-		//System.out.println("Hi this is a change !!");
-		//System.out.println("Hi this is a change again !!");
-		//System.out.println("Hi this is the third change !!");
-		
+		System.out.println("Hi this is a change !!");
+		System.out.println("Hi this is a change again !!");
+		System.out.println("Hi this is the third change !!");
+		System.out.println("Interactive Rebase !!!");
 	}
 
 }
