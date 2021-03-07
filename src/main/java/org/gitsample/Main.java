fb9f43af2b209d97495384f62e9a315ad4fe66c5
Vaibhav <vaibhav0320@yahoo.co.in>
2021-03-07 14:57:42 +0530

    Modified Main.java

--- src/main/java/org/gitsample/Main.java
+++ src/main/java/org/gitsample/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		System.out.println("Hi this is a change !!");
 		System.out.println("Hi this is a change again !!");
 		System.out.println("Hi this is the third change !!");
+		System.out.println("Another change, this is fourth change !!");
 		System.out.println("Interactive Rebase !!!");
 	}
 
